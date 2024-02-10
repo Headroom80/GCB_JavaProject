9e9437e1372281c6e66555887bce1f18b9e2e301
Headroom80 <Czzero63@gmail.com>
2024-02-10 13:19:02 +0100

    add 3 function to TestDoWhile

--- TestDoWhile.java
+++ TestDoWhile.java
@@ -9,14 +9,7 @@ public class TestDoWhile {
         Scanner lectureClavier = new Scanner(System.in);
 
         do {
-            System.out.println(" 1. Création d'un compte");
-            System.out.println(" 2. Affichage d'un compte");
-            System.out.println(" 3. Ecrire une ligne comptable");
-            System.out.println(" 4. Sortir");
-            System.out.println(" 5. De l'aide");
-            System.out.println();
-            System.out.print(" Votre choix : ");
-            choix = lectureClavier.nextByte();
+           choix = menuPrincipal();
             switch (choix){
                 case 1 : do{
                     System.out.print("Type du compte [Types possibles :" );
@@ -52,23 +45,42 @@ public class TestDoWhile {
                     System.out.println("Option non programmée");
                     break;
                 case 4 :
-                    System.out.println("Au revoir et à bient0t");
-                    System.exit(0) ;
+                    sortir();
                     break;
                 case 5 :
-                    System.out.println("Option 1. Pour creer un compte Courant entrer C ");
-                    System.out.println("          Pour creer un compte Joint entrer J ");
-                    System.out.println("          Pour creer un compte Epargne entrer E");
-                    System.out.print("          Puis, entrer le numero du compte, et");
-                    System.out.println(" sa premiere valeur creditee ");
-                    System.out.println("           Dans le cas d'un compte epargne, entrer le taux ");
-                    System.out.println("Option 2. Le systeme affiche les donnees du compte choisi ");
-                    System.out.println("Option 3. Ecrire une ligne comptable");
-                    System.out.println("Option 4. Pour quitter le programme");
-                    System.out.println("Option 5. Pour afficher de l'aide");
+                    aLaide();
             }
         } while(choix != 4);
         System.out.println("Au revoir et à bient0t");
         System.exit(0) ;
+    }
+    public static byte menuPrincipal(){
+        byte tmp;
+        Scanner lectureClavier = new Scanner(System.in);
+            System.out.println(" 1. Création d'un compte");
+            System.out.println(" 2. Affichage d'un compte");
+            System.out.println(" 3. Ecrire une ligne comptable");
+            System.out.println(" 4. Sortir");
+            System.out.println(" 5. De l'aide");
+            System.out.println();
+            System.out.print(" Votre choix : ");
+            tmp = lectureClavier.nextByte();
+        return tmp;
+    }
+    public  static void aLaide(){
+        System.out.println("Option 1. Pour creer un compte Courant entrer C ");
+        System.out.println("          Pour creer un compte Joint entrer J ");
+        System.out.println("          Pour creer un compte Epargne entrer E");
+        System.out.print("          Puis, entrer le numero du compte, et");
+        System.out.println(" sa premiere valeur creditee ");
+        System.out.println("           Dans le cas d'un compte epargne, entrer le taux ");
+        System.out.println("Option 2. Le systeme affiche les donnees du compte choisi ");
+        System.out.println("Option 3. Ecrire une ligne comptable");
+        System.out.println("Option 4. Pour quitter le programme");
+        System.out.println("Option 5. Pour afficher de l'aide");
+    }
+    public static void sortir(){
+        System.out.println(" Aurevoir et à bientôt ");
+        System.exit(0);
     }
 }
